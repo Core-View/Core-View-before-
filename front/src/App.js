b1428c1dfe51428dcd4076bf29cc769ff97c1a19
hnk1203 <hnk1203@naver.com>
2024-05-27 01:49:42 +0900

    import Header from "./Common/header"; => import Header from "./Common/Header"; 로 수정함

--- front/src/App.js
+++ front/src/App.js
@@ -1,8 +1,9 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './Common/header';
-import Main from './Home/home_main';
-import Mypage from './My/my_main';
-import SignIn from './Sign/signin_main';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// header 여서 안나왔는데 Header 로 고쳐서 나옵니다 이제
+import Header from "./Common/Header";
+import Main from "./Home/home_main";
+import Mypage from "./My/my_main";
+import SignIn from "./Sign/signin_main";
 
 function App() {
   return (
