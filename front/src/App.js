89770e7fcac44ae0e2f3ac172df6120f5c651cdf
kimjin27 <heajin2530@naver.com>
2024-05-27 22:09:02 +0900

    마이페이지 경호수정

--- front/src/App.js
+++ front/src/App.js
@@ -12,7 +12,7 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Main />} />
-          <Route path="mypage" element={<Mypage />} />
+          <Route path="my_main" element={<Mypage />} />
           <Route path="users">
             <Route path="sign-in" element={<SignIn />} />
           </Route>
