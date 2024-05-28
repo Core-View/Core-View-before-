146dc6728fcdd1187170660507c4f47323c775c5
hnk1203 <hnk1203@naver.com>
2024-05-28 13:41:57 +0900

    메인 기본틀

--- front/src/Home/home_main.jsx
+++ front/src/Home/home_main.jsx
@@ -1,9 +1,45 @@
-import React from 'react';
-
+import React from "react";
+import "./home_main.css";
 const Main = () => {
   return (
-    <div>
-      <h1>잘나오나요</h1>
+    <div className="home-container">
+      <section className="home_left">
+        <div>기여도 순위</div>
+        <ul className="border">
+          <li>1. 이브라히모비치</li>
+          <li>2. 이브라히모비치</li>
+          <li>3. 이브라히모비치</li>
+          <li>4. 이브라히모비치</li>
+          <li>5. 이브라히모비치</li>
+          <li>6. 이브라히모비치</li>
+          <li>7. 이브라히모비치</li>
+          <li>8. 이브라히모비치</li>
+          <li>9. 이브라히모비치</li>
+          <li>10. 이브라히모비치</li>
+        </ul>
+      </section>
+      <section className="home_mid">
+        <div className="post recommend">
+          <div>추천게시글</div>
+          <ul className="border">
+            <li>개쩌는 코드1</li>
+            <li>개쩌는 코드2</li>
+            <li>개쩌는 코드3</li>
+          </ul>
+        </div>
+        <div className="post newest">
+          <div>최신게시글</div>
+          <ul className="border">
+            <li>개쩌는 코드1</li>
+            <li>개쩌는 코드2</li>
+            <li>개쩌는 코드3</li>
+          </ul>
+        </div>
+      </section>
+      <section className="home_right">
+        <div>🚀</div>
+        <div>전체게시글보기</div>
+      </section>
     </div>
   );
 };
