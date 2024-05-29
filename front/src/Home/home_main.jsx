f9f924865c33477f4ea85af44feacf296df083ce
hnk1203 <hnk1203@naver.com>
2024-05-30 03:48:16 +0900

    홈 레이아웃 90%

--- front/src/Home/home_main.jsx
+++ front/src/Home/home_main.jsx
@@ -4,24 +4,26 @@ const Main = () => {
   return (
     <div className="home-container">
       <section className="home_left">
-        <div>기여도 순위</div>
-        <ul className="border">
-          <li>1. 이브라히모비치</li>
-          <li>2. 이브라히모비치</li>
-          <li>3. 이브라히모비치</li>
-          <li>4. 이브라히모비치</li>
-          <li>5. 이브라히모비치</li>
-          <li>6. 이브라히모비치</li>
-          <li>7. 이브라히모비치</li>
-          <li>8. 이브라히모비치</li>
-          <li>9. 이브라히모비치</li>
-          <li>10. 이브라히모비치</li>
-        </ul>
+        <div className="post rank">
+          <div>기여도 순위</div>
+          <ul className="rankcode">
+            <li>1. 이브라히모비치</li>
+            <li>2. 이브라히모비치</li>
+            <li>3. 이브라히모비치</li>
+            <li>4. 이브라히모비치</li>
+            <li>5. 이브라히모비치</li>
+            <li>6. 이브라히모비치</li>
+            <li>7. 이브라히모비치</li>
+            <li>8. 이브라히모비치</li>
+            <li>9. 이브라히모비치</li>
+            <li>10. 이브라히모비치</li>
+          </ul>
+        </div>
       </section>
       <section className="home_mid">
         <div className="post recommend">
           <div>추천게시글</div>
-          <ul className="code border">
+          <ul className="postcode">
             <li>개쩌는 코드1</li>
             <li>개쩌는 코드2</li>
             <li>개쩌는 코드3</li>
@@ -29,7 +31,7 @@ const Main = () => {
         </div>
         <div className="post newest">
           <div>최신게시글</div>
-          <ul className="code border">
+          <ul className="postcode">
             <li>개쩌는 코드1</li>
             <li>개쩌는 코드2</li>
             <li>개쩌는 코드3</li>
@@ -37,8 +39,10 @@ const Main = () => {
         </div>
       </section>
       <section className="home_right">
-        <div>🚀</div>
-        <div>전체게시글보기</div>
+        <div>
+          <div>🚀</div>
+          <div>게시판</div>
+        </div>
       </section>
     </div>
   );
