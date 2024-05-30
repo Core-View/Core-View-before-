145fff4c6dd085b46e1c82c250dd48d3eaa6f333
kimjin27 <heajin2530@naver.com>
2024-05-30 23:29:30 +0900

    마이 페이지

--- front/src/My/my_main.jsx
+++ front/src/My/my_main.jsx
@@ -1,9 +1,48 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './my_main.css';
 
 const Mypage = () => {
   return (
     <div>
-      <h1>my_main</h1>
+      <div className='alram'>
+        <img src="ring.png" alt="alram" className='alram'/>
+      </div>
+      <div className='space1'>
+        <div className='my_info'>
+          <div className='image_profile'>
+            <img src="original_profile.png" alt="profile" className='my_profile'/>
+          </div>
+          <div className='info_zone'>
+            <div className='name_zone'>
+              <h5>이름</h5>
+            </div>
+            <div className='intro_zone'>
+              <h5>소개</h5>
+            </div>
+          </div>
+        </div>
+        <div className='activity'>
+          <div className='good'>
+            <h5>좋아요</h5>
+          </div>
+          <div className='contribution'>
+            <h5>기여도</h5>
+          </div>
+        </div>
+      </div>
+      <div className='space2'>
+        <ul className='menu_bar'>
+          <li className='menu_item'>
+            <Link to="/my_posting" style={{textDecoration: "none", color:'black'}}>게시글</Link>
+          </li>
+          <li className='menu_item'>
+            <Link to="/my_comment" style={{textDecoration: "none", color:'black'}}>댓글</Link>
+          </li>
+        </ul>
+        <hr/>
+        <h1>hello</h1>
+      </div>
     </div>
   );
 };
---
+++ front/src/My/my_modify.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Mymodify = () => {
+  return (
+    
+  );
+};
+
+export default Mymodify;
