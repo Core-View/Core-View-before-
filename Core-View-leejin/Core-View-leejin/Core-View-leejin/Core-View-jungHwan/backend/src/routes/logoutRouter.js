9a5c1da20333aaf2774d70a16edb13f74c40236f
JANGYUN <45096953+JANGYUN@users.noreply.github.com>
2024-06-02 00:09:31 +0900

    Delete Core-View-leejin/Core-View-leejin/Core-View-leejin directory

--- Core-View-leejin/Core-View-leejin/Core-View-leejin/Core-View-jungHwan/backend/src/routes/logoutRouter.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const logoutController = require("../controllers/logoutController");
-
-router.post("/", logoutController.logout);
-
-module.exports = router;
