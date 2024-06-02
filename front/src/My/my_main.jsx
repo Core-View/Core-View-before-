6d9875cf5f570bccaa96d49e8225165ad745ba0b
kimjin27 <heajin2530@naver.com>
2024-06-02 17:17:35 +0900

    마이페이지 수정

--- front/src/My/my_main.jsx
+++ front/src/My/my_main.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './my_main.css';
 import Modal from './my_alram';
@@ -10,6 +10,44 @@ const Mypage = () => {
     { id: 2, text: "새로운 친구 요청이 있습니다", read: false },
     { id: 3, text: "이벤트 참여 확인", read: false }
   ]);
+
+  // JSON 데이터로부터 사용자 정보를 받아오기 위한 상태 추가
+  const [userInfo, setUserInfo] = useState({ name: '', intro: '' , profileImg: '' });
+  const [posts, setPosts] = useState([]);
+  const [comments, setComments] = useState([]); // 댓글 단 게시글 제목 저장
+  // 가상의 API로부터 데이터를 로드한다고 가정
+  useEffect(() => {
+    const fetchUserData = async () => {
+      // API 호출 대신 정적 JSON 데이터 사용
+      const data = await Promise.resolve({
+        name: "홍길동",
+        intro: "안녕하세요, React를 좋아하는 개발자입니다.",
+        profileImg : "ring.png" //이미지
+      });
+      setUserInfo(data);
+    };
+
+    const fetchPostsData = async() => {
+      const postsData = await Promise.resolve([
+        { id: 1, title: "게시글 1" },
+        { id: 2, title: "게시글 2" },
+        { id: 3, title: "게시글 3" }
+      ]);
+      setPosts(postsData);
+    };
+    const fetchCommentsData = async () => {
+      const commentsData = await Promise.resolve([
+        { id: 1, title: "게시글에 대한 댓글 1" },
+        { id: 2, title: "게시글에 대한 댓글 2" },
+        { id: 3, title: "게시글에 대한 댓글 3" }
+      ]);
+      setComments(commentsData);
+    };
+    fetchUserData();
+    fetchPostsData();
+    fetchCommentsData();
+  }, []);
+
   const toggleModal = () => {
     setModalOpen(!modalOpen);
     setNotifications(notifications.map(notification => ({ ...notification, read: true })));
@@ -34,14 +72,14 @@ const Mypage = () => {
       <div className='space1'>
         <div className='my_info'>
           <div className='image_profile'>
-            <img src="original_profile.png" alt="profile" className='my_profile'/>
+            <img src={userInfo.profileImg || "original_profile.png"} alt="profile" className='my_profile'/>
           </div>
           <div className='info_zone'>
             <div className='name_zone'>
-              <h5>이름</h5>
+              <h5>{userInfo.name}</h5>
             </div>
             <div className='intro_zone'>
-              <h5>소개</h5>
+              <h5>{userInfo.intro}</h5>
             </div>
             <div className='modify_zone'>
               <Link to='/my_modify' style={{textDecoration: "none", color:'black'}}>정보수정</Link>
@@ -58,16 +96,50 @@ const Mypage = () => {
         </div>
       </div>
       <div className='space2'>
-        <ul className='menu_bar'>
-          <li className='menu_item'>
+        <div className='post_zone'>
+          <div className='my_posts'>
             <Link to="/my_posting" style={{textDecoration: "none", color:'black'}}>게시글</Link>
-          </li>
-          <li className='menu_item'>
+          </div>
+          <hr></hr>
+          <div>
+            <h1>게시글 모임</h1>
+            {posts.length > 0 ? (
+            <ul>
+              {posts.map(post => (
+                <li key={post.id}>
+                  <Link to={`/post/${post.id}`} style={{textDecoration: "none", color:'black'}}>
+                    {post.title}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+            ) : (
+              <p>게시글이 없습니다.</p>
+            )}
+          </div>
+        </div>
+        <div className='comment_zone'>
+          <div className='my_comments'>
             <Link to="/my_comment" style={{textDecoration: "none", color:'black'}}>댓글</Link>
-          </li>
-        </ul>
-        <hr/>
-        <h1>hello</h1>
+          </div>
+          <hr></hr>
+          <div>
+          <h1>내가 댓글 단 게시글</h1>
+            {comments.length > 0 ? (
+            <ul>
+              {comments.map(comment => (
+                <li key={comment.id}>
+                  <Link to={`/post/${comment.id}`} style={{textDecoration: "none", color:'black'}}>
+                    {comment.title}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+            ) : (
+              <p>댓글 단 게시글이 없습니다.</p>
+            )}
+          </div>
+        </div>
       </div>
     </div>
   );
